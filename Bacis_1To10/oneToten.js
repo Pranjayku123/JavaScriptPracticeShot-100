f42ca30fca977fcd9549bd1d608904975323ff9c
#DEVprk <109287745+Pranjayku123@users.noreply.github.com>
2025-02-17 09:43:51 +0000

    4 questions done out of 10 now

---
+++ Bacis_1To10/oneToten.js
@@ -0,0 +1,84 @@
+// WAP that reverse the string "Pranjay"
+// soluation-1
+let orignalStr = "Pranjay";
+let reverseStr = "";
+for(let i = orignalStr.length-1; i >= 0; i--){
+    reverseStr += orignalStr[i];   
+}
+// console.log(reverseStr);
+// soluation -2
+reverseStr= orignalStr.split('').reverse().join('');
+// console.log(reverseStr);
+
+// WAP To Find Duplicate Elements in an Array
+const arr = [1, 2, 3, 4, 2, 3, 5, 6, 3];
+function findDuplicate(data) {
+    let unique = new Set();
+    let duplicates = [];
+    data.forEach(element => {
+        if (unique.has(element)) {
+            duplicates.push(element);
+        } else {
+            unique.add(element);
+        }
+    });
+
+    return duplicates;
+}
+
+
+console.log(findDuplicate(arr)); 
+
+
+// WAP To Check If a String is a Palindrome
+
+// soluation-1
+let str = "DAD";
+// let reverseString = str.split("").reverse().join("");
+let reverseString = "";
+for (let i = str.length -1; i >= 0; i--) {
+   reverseString += str[i];
+}
+console.log(reverseString);
+
+// console.log(reverseString);
+if(reverseString === str){
+    // console.log(`${reverseString} is Palindrome String`);
+    
+}else{
+// console.log(`${str} is Not Palindrome String`);
+}
+
+
+// WAP to count Vovel in String
+const vovelString = "Pranjay Singh";
+const Vowel = "aeiouAEIOU";
+
+function vowelCountFn(str) {
+    let vovelCount = 0;
+
+    str.split('').forEach(char => {
+        if (Vowel.includes(char)) {  // Check if the character is in the vowel list
+            vovelCount++;
+        }
+    });
+
+    return vovelCount;
+}
+
+// console.log(vowelCountFn(vovelString)); 
+
+
+
+
+
+ 
+
+
+ 
+    
+ 
+ 
+
+
+
